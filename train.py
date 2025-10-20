0f179300bb2259e6a74ac6cf99374d2f70ed4999
Hana <h.hatem2262@nu.edu.eg>
2025-10-20 09:04:11 +0300

    Experiment: SVC model

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import joblib
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 # Load training data
@@ -9,8 +9,8 @@ train_data = pd.read_csv('data/train.csv')
 X_train = train_data.drop('Survived', axis=1)
 y_train = train_data['Survived']
 
-# Train baseline model (LogisticRegression)
-model = LogisticRegression(max_iter=1000, random_state=42)
+# Train baseline model (SVC)
+model = SVC(kernel='rbf', random_state=42)
 model.fit(X_train, y_train)
 
 # Save model
